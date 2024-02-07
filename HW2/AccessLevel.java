f172a1e9118f86bc81dafe0e4e5881e42ce388be
HJev <157658614+HJev@users.noreply.github.com>
2024-02-07 11:52:39 -0500

    Update AccessLevel.java

--- HW2/AccessLevel.java
+++ HW2/AccessLevel.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Vector;
 public class AccessLevel {
-    public static String Access(int[] rights, int minPermission){
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Vector<Integer> nums = new Vector<Integer>();
+        System.out.println("Enter a list of numbers separated by comas for the rights:");
+        String test = scan.nextLine();
+        System.out.println("Enter the minimum requirements");
+        int min = scan.nextInt();
+        scan.close();
+        scan = new Scanner(test);
+        scan.useDelimiter(",");
+        while (scan.hasNext()){
+            nums.add(scan.nextInt());
+        }
+        scan.close();
+
+        System.out.println(Access(nums.toArray(new Integer[]{}), min));
+        
+
+    }
+    public static String Access(Integer[] rights, int minPermission){
         String permission = "";
         for(int i = 0; i < rights.length; i++){
             if(rights[i] < minPermission)
